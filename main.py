a1fafdb81a9b045cb6e6cc0f64a7ad2f61e322b5
chrisf725 <chfrancisco725@yahoo.com>
2025-04-28 23:48:22 -0700

    Improved color checking; improved pause functionality with the addition of smart_sleep

--- main.py
+++ main.py
@@ -28,8 +28,25 @@ if plat_train_response not in ["yes", "y", "no", "n"]:
 plat_train = plat_train_response == "yes" or plat_train_response == "y"
 print(f"Platinum train: {plat_train}")
 
+bypass_rare_response = input("Do you want to ignore rare crits? (yes/no/y/n): ").strip().lower()
+if bypass_rare_response not in ["yes", "y", "no", "n"]:
+    print("Invalid input. Please restart the script and enter 'yes', 'no', 'y', or 'n'.")
+    exit()
+
+bypass_rare = bypass_rare_response == "yes" or bypass_rare_response == "y"
+print(f"Ignore rare crits: {bypass_rare}")
+
 # Store coordinates of search object
 click_coordinates = []
+
+def smart_sleep(total_time, interval=0.1):
+    elapsed = 0
+    while elapsed < total_time:
+        # If paused, keep waiting (in short increments)
+        while paused:
+            time.sleep(interval)
+        time.sleep(interval)
+        elapsed += interval
 
 paused = False
 def toggle_pause():
@@ -77,7 +94,8 @@ def click_action(x, y, time_delay):
     pyautogui.mouseDown(window.left + x, window.top + y)
     time.sleep(0.01)
     pyautogui.mouseUp()
-    time.sleep(time_delay)
+    # time.sleep(time_delay)
+    smart_sleep(time_delay)
 
 def get_pixel_color(x, y):
     screenshot = pyautogui.screenshot()
@@ -85,15 +103,29 @@ def get_pixel_color(x, y):
     del screenshot  # Free up memory
     return pixel_color
 
-def is_blue_pixel(color, blue_threshold=150, other_max=100):
+def is_blue_pixel(color, blue_threshold=150, other_max=130):
     r, g, b = color
     return b > blue_threshold and r <= other_max and g <= other_max
+
+def is_cyan_pixel(color, cyan_threshold=155, other_max=170):
+    r, g, b = color
+    return r <= other_max and g > cyan_threshold and b > cyan_threshold
+
+def is_yellow_pixel(color, yellow_threshold=150, other_max=100):
+    r, g, b = color
+    return r > yellow_threshold and g > yellow_threshold and b <= other_max
+
+def is_dark_grey_pixel(color, target=(46, 57, 69), tolerance=10):
+    r, g, b = color
+    tr, tg, tb = target
+    return abs(r - tr) <= tolerance and abs(g - tg) <= tolerance and abs(b - tb) <= tolerance
 
 print("Right click on the object you want to search for")
 with mouse.Listener(on_click=on_click) as listener:
     listener.join()
 
-time.sleep(3)
+# time.sleep(3)
+smart_sleep(3)
 print(f"Click coordinates: {click_coordinates}")
 
 try:
@@ -106,7 +138,8 @@ try:
         for coord in click_coordinates:
             # Move the mouse to the search object and click
             pyautogui.moveTo(window.left + coord[0], window.top + coord[1])
-            time.sleep(1)
+            # time.sleep(1)
+            smart_sleep(1)
             click_action(coord[0], coord[1], 4)
             print(f"Clicked at ({coord[0]}, {coord[1]})")
 
@@ -121,7 +154,8 @@ try:
             color = get_pixel_color(check_x, check_y)
             print(f"Color at ({check_x}, {check_y}): {color}")
             pyautogui.moveTo(check_x, check_y)
-            time.sleep(1)
+            # time.sleep(1)
+            smart_sleep(1)
 
             if exit_flag:
                 print("Exiting... 2")
@@ -131,25 +165,29 @@ try:
                 # check_exit()
 
                 result_color = get_pixel_color(window.left + 272, window.top + 122)
-                result_color_2 = get_pixel_color(window.left + 459, window.top + 279)
+                # result_color_2 = get_pixel_color(window.left + 459, window.top + 279)
+                result_color_2 = get_pixel_color(window.left + 446, window.top + 276)
 
                 # pyautogui.moveTo(window.left + 272, window.top + 122)
                 # Continue script if crit is common
                 # End script if crit is rare and above
-                if color == (98, 98, 98): # Grey
+                if color == (98, 98, 98) or (bypass_rare and is_blue_pixel(color)): # Grey
                     # check_exit()
-                    print(result_color_2)
-                    time.sleep(1)
-                    click_action(328, 621, 1)
+                    print("XP bar color: ", result_color_2)
+                    # time.sleep(1)
+                    smart_sleep(1)
+                    click_action(328, 621, 2)
+                    # pyautogui.moveTo(window.left + 446, window.top + 276)
 
                     
                     # pyautogui.moveTo(window.left + 459, window.top + 279)
                     # time.sleep(1)
                     
                     # Check if crit is ready to train
-                    if result_color_2 == (255, 255, 255) and result_color == (107, 138, 19): # White and Green
+                    if is_yellow_pixel(result_color_2): # and result_color == (107, 138, 19): # White and Green
                         # check_exit()
-                        time.sleep(1)
+                        # time.sleep(1)
+                        smart_sleep(1)
                         # Click continue on results screen
                         click_action(574, 591, 1)
 
@@ -172,7 +210,8 @@ try:
 
                             result_color_3 = get_pixel_color(window.left + 464, window.top + 108)
                             print("Evolution", result_color_3)
-                            time.sleep(1)
+                            # time.sleep(1)
+                            smart_sleep(1)
 
                             # Check if crit evolved
                             if result_color_3 == (107, 138, 19):
@@ -184,7 +223,8 @@ try:
 
                             result_color_4 = get_pixel_color(window.left + 593, window.top + 195)
                             print("Rank upgrade", result_color_4)
-                            time.sleep(1)
+                            # time.sleep(1)
+                            smart_sleep(1)
 
                             # Close rank upgrade screen
                             if result_color_4 == (107, 138, 19):
@@ -200,7 +240,8 @@ try:
 
                             result_color_3 = get_pixel_color(window.left + 464, window.top + 108)
                             print("Evolution", result_color_3)
-                            time.sleep(1)
+                            # time.sleep(1)
+                            smart_sleep(1)
 
                             # Check if crit evolved
                             if result_color_3 == (107, 138, 19):
@@ -212,16 +253,18 @@ try:
 
                             result_color_4 = get_pixel_color(window.left + 593, window.top + 195)
                             print("Rank upgrade", result_color_4)
-                            time.sleep(1)
+                            # time.sleep(1)
+                            smart_sleep(1)
 
                             # Close rank upgrade screen
                             if result_color_4 == (107, 138, 19):
                                 click_action(589, 511, 1) # Close button
                         break
                     # If crit is not ready to train, continue
-                    if result_color_2 == (94, 108, 126): # Grey
+                    if is_blue_pixel(result_color_2) or is_dark_grey_pixel(result_color_2) or is_cyan_pixel(result_color_2): # result_color_2 == (94, 108, 126): # Grey
                         # check_exit()
-                        time.sleep(1)
+                        # time.sleep(1)
+                        smart_sleep(1)
                         # Click continue on results screen
                         click_action(574, 591, 0)
                         break
